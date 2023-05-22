9a5e756f15aeaac6771b850f563983e6ca442280
Spolito24 <[baptisterenier69@gmail.com](mailto:baptisterenier69@gmail.com)>
2023-05-22 16:33:18 +0200

    done quest 08

--- migrations/Version20230522142209.php
+++ migrations/Version20230522142209.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230522111058 extends AbstractMigration
+final class Version20230522142209 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20230522111058 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE program DROP name');
+        $this->addSql('ALTER TABLE program ADD category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED778412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED778412469DE2 ON program (category_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE program ADD name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED778412469DE2');
+        $this->addSql('DROP INDEX IDX_92ED778412469DE2 ON program');
+        $this->addSql('ALTER TABLE program DROP category_id');
     }
 }
